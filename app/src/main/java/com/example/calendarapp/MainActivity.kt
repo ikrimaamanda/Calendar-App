875317158900dfcfe02e14c1328a5f68db78132c
Ikrima Amanda <ikrima.amanda.07@gmail.com>
2021-02-18 11:44:13 +0800

    updated

--- app/src/main/java/com/example/calendarapp/MainActivity.kt
+++ app/src/main/java/com/example/calendarapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.calendarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Contacts
+import androidx.databinding.DataBindingUtil
+import com.example.calendarapp.databinding.ActivityMainBinding
+import com.jonaswanke.calendar.Event
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
